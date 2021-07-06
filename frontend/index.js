56110906f4c66a771241375a5a7ebe260ece54b4
hemangandhi <hemangandhi@gmail.com>
2021-07-06 00:49:47 -0400

    Added a board and drag and drop on the frontend

---
+++ frontend/index.js
@@ -0,0 +1,155 @@
+// Koma states:
+// 1) kakushi-koma (still in the bag)
+// 2) mochi-koma (in a player's hand)
+// 3) tanin-koma (seeing somebody else's pieces)
+// 4) tameshi-koma (being tried on the board)
+// 5) utta-koma
+//
+// Transitions:
+// 1->2: dealt a koma
+// 2->3: share hand with others
+// 2->4: try to play from a closed hand
+// 3->4: try to play from an open hand (or somebody else does)
+// 4->2: cancel a play with a closed hand
+// 4->3: cancel a play with an open hand
+// 4->5: commit a play
+//
+// Interaction with dragging:
+// - Unoccupied board cells are dropped into for the transitions into 4.
+
+var KomaState = {
+    KAKUSHI: 1,
+    MOCHI: 2,
+    TANIN: 4,
+    TAMESHI: 8,
+    UTTA:16
+};
+
+function Koma(label, score, state, is_public) {
+    this.label = label;
+    this.score = score;
+    this.state = state;
+    // If state was ever KomaState.TANIN
+    this.is_public = is_public;
+}
+
+var currently_dragged = null;
+var current_drop = null;
+
+function KomaContainer(hand_elt, nkoma_limit, for_koma_state) {
+
+    this.koma_list = [];
+    this.limit = nkoma_limit;
+    this.n_koma = 0;
+
+    var _this = this;
+    function makeKomaDraggable(koma, koma_elt) {
+	koma_elt.setAttribute('draggable', true);
+	koma_elt.addEventListener('dragstart', function(evt) {
+	    evt.dataTransfer.setData("text", JSON.stringify(koma));
+	    evt.effectAllowed = "move";
+	    evt.dataTransfer.dropEffect = "move";
+	    _this.n_koma--;
+	    for (var i = 0; i < _this.koma_list.length; i++) {
+		if (_this.koma_list[i] == koma) {
+		    _this.koma_list.splice(i, 1);
+		    break;
+		}
+	    }
+	    currently_dragged = koma;
+	});
+	koma_elt.addEventListener('dragend', function(evt) {
+	    evt.dataTransfer.clearData();
+	    // This means we've yet to drop, so we just want to cancel the drag.
+	    if (currently_dragged != null) {
+		if (current_drop != null) {
+		    current_drop.style.border = "solid grey";
+		    current_drop = null;
+		}
+		return;
+	    }
+	    // Weirdness: this is for slightly better effects since we don't want the element to vanish when the user starts dragging it? 
+	    hand_elt.removeChild(koma_elt);
+	});
+    }
+
+    // Make sure that when we're returning a koma to a hand, we're putting public ones
+    // publically and private ones privately.
+    // (NOTE: this split will not exist for the person who's sharing their hand.)
+    function validKomaState(koma) {
+	if (koma.is_public)
+	    return (KomaState.MOCHI & for_koma_state) == 0
+	else
+	    return (KomaState.TANIN & for_koma_state) == 0
+    }
+
+    this.addKoma = function(koma) {
+	if (this.n_koma == this.limit || !validKomaState(koma)) return;
+	this.n_koma++;
+	var koma_elt = document.createElement('div');
+	koma_elt.innerText = koma.label + '(' + koma.score + ')';
+	koma_elt.classList.add('koma-tag');
+	// Expectation: we shouldn't see a kakushi koma here.
+	// 17 = KomaState.KAKUSHI | Komastate.UTTA
+	if ((for_koma_state & 17) === 0) {
+	    makeKomaDraggable(koma, koma_elt);
+	}
+	hand_elt.appendChild(koma_elt);
+	this.koma_list.push(koma);
+    }
+
+    hand_elt.addEventListener('drop', function(evt) {
+	evt.preventDefault();
+	var koma = JSON.parse(evt.dataTransfer.getData('text'));
+	koma.state = for_koma_state;
+	_this.addKoma(koma);
+	currently_dragged = null;
+    });
+
+    hand_elt.addEventListener('dragenter', function(evt) {
+	hand_elt.style.border = "solid black";
+	current_drop = hand_elt;
+	evt.preventDefault();
+    });
+    hand_elt.addEventListener('dragover', function(evt) {
+	evt.preventDefault();
+    });
+    hand_elt.addEventListener('dragleave', function(evt) {
+	hand_elt.style.border = "solid grey";
+	current_drop = null;
+	evt.preventDefault();
+    });
+}
+
+function BoardSquare(sq_elt, row, col, word_mult, letter_mult) {
+    KomaContainer.call(this, sq_elt, 1, KomaState.TAMESHI);
+    sq_elt.classList.add('cell');
+    if (word_mult > 1) {
+	sq_elt.classList.add('word-bonus');
+	sq_elt.innerText = word_mult + 'W';
+    }
+    if (letter_mult > 1) {
+	sq_elt.classList.add('letter-bonus');
+	sq_elt.innerText = letter_mult + 'L';
+    }
+
+    this.word_mult = word_mult;
+    this.letter_mult = letter_mult;
+    this.row = row;
+    this.col = col;
+
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    var cells = [];
+    installBoard(document.getElementById('board'), function(sq_tag, sq_info) {
+	sq_tag.id = 'cell-' + sq_info[0] + '-' + sq_info[1];
+	cells.push(new BoardSquare(sq_tag, ...sq_info));
+    });
+
+    var hand_elt = document.getElementById('my-tiles');
+    var hand_container = new KomaContainer(hand_elt, 7, KomaState.MOCHI);
+    for(var i = 0; i < 7; i++) {
+	hand_container.addKoma(new Koma(i, i, KomaState.MOCHI, false));
+    }
+});
---
+++ frontend/scrabble-board.js
@@ -0,0 +1,65 @@
+var BOARD_SIZE = 15;
+
+function extendOverQuandrants(top_left_squares) {
+    var squares = [];
+    for(var q = 0; q < 4; q++) {
+	for(var i = 0; i < top_left_squares.length; i++) {
+	    var sq = top_left_squares[i];
+	    var r = (q % 2 != 0)? BOARD_SIZE - 1 - sq[0] : sq[0];
+	    var c = (q >= 2)? BOARD_SIZE - 1 - sq[1] : sq[1];
+	    squares.push([r, c, sq[2], sq[3]]);
+	}
+    }
+
+    // sort | uniq
+    return squares.sort(function(l, r) {
+	var c = l[0] - r[0];
+	if (c != 0) return c;
+	return l[1] - r[1];
+    }).filter(function (sq, pos, arr) {
+	return pos == 0 || sq[0] != arr[pos - 1][0] || sq[1] != arr[pos - 1][1];
+    });
+}
+
+// (row, column, word_multiplier, letter_multiplier)
+// NOTE: this is sorted by row then column where each (row, column) appears at most once.
+var SPECIAL_SQUARES = extendOverQuandrants([
+    [0, 0, 3, 1],
+    [0, 3, 1, 2],
+    [0, 7, 3, 1],
+    [1, 1, 2, 1],
+    [1, 5, 1, 3],
+    [2, 2, 2, 1],
+    [2, 6, 1, 2],
+    [3, 0, 1, 2],
+    [3, 3, 2, 1],
+    [3, 7, 1, 2],
+    [4, 4, 2, 1],
+    [5, 1, 1, 3],
+    [5, 5, 1, 3],
+    [6, 2, 1, 2],
+    [6, 6, 1, 2],
+    [7, 0, 3, 1],
+    [7, 3, 1, 2],
+    [7, 7, 2, 1]
+]);
+
+function installBoard(table_tag, square_ctor) {
+    var special_square_idx = 0;
+    for(var i = 0; i < BOARD_SIZE; i++) {
+	var row = document.createElement('tr');
+	for(var j = 0; j < BOARD_SIZE; j++) {
+	    var square = document.createElement('td');
+	    if (special_square_idx < SPECIAL_SQUARES.length
+		&& i == SPECIAL_SQUARES[special_square_idx][0]
+		&& j == SPECIAL_SQUARES[special_square_idx][1]) {
+		square_ctor(square, SPECIAL_SQUARES[special_square_idx]);
+		special_square_idx++;
+	    } else {
+		square_ctor(square, [i, j, 1, 1]);
+	    }
+	    row.appendChild(square);
+	}
+	table_tag.appendChild(row);
+    }
+}
